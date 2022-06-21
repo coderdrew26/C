ade6b8d3078325b7a7d344220a57cd5a50863a23
coderdrew26 <josephandrewnepomuceno@gmail.com>
2022-06-21 17:08:31 +0800

    commit

--- whileskip.c
+++ whileskip.c
@@ -9,9 +9,12 @@ int main() {
 
   while (i < 10) {
     // Figure out how to skip the print of 5 here
- 
-  }
+    if (i == 5) {
+    i++; 
+    continue;
+    }
 
     printf("%d\n", i);
     i++;
   }
+}
